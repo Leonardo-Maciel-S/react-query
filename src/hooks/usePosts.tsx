6932cc038655c8fa4719a64d4c006e29fe34afa3
Leonardo Maciel <leonardomacielj@gmail.com>
2025-03-15 12:11:03 -0300

    refactor: changed fetcy to axios api

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,17 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import IPost from "../interfaces/post";
+import { api } from "../axios/axios";
 
 const getPosts = (): Promise<IPost[]> => {
-  return fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => data)
-    .catch((e) => console.log(e));
+  console.log("teste");
+  return api
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.data);
 };
 
 export function usePosts() {
   const query = useQuery({
     queryKey: ["posts"],
     queryFn: getPosts,
+    staleTime: 5 * 60 * 1000,
   });
 
   return query;
